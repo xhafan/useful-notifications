9a223d5d14da79e2dbec39ec8ced06153d9abbbc
xhafan <xhafan@gmail.com>
2019-01-28 16:18:48 +0100

    Changed the wording to Czech language - the initial useful notification will be notifying about good films in Czech cinemas

---
+++ src/UsefulNotifications.Website/Controllers/FilmsWithGoodRatingNotificationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UsefulNotifications.Website.Controllers
+{
+    public class FilmsWithGoodRatingNotificationController : Controller
+    {
+        public IActionResult Index() // todo: test me
+        {
+            return View();
+        }
+    }
+}
